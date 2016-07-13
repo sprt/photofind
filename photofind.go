1109b138e76d212fa1e8526e9fce502e80cbeb80
sprt <hellosprt@gmail.com>
2016-07-14 01:23:43 +0200

    Initial commit

---
+++ photofind.go
@@ -0,0 +1,14 @@
+package photofind
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "photofind")
+}
